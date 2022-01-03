00b6d98dc9a99de7e82944d013391d00da2160b7
Ravish Rajput <ravish.rajput22@gmail.com>
2022-01-04 00:45:18 +0530

    Finalising combined button UI Component

--- app/src/main/java/com/ravish/compose/sample/screens/ButtonsScreen.kt
+++ app/src/main/java/com/ravish/compose/sample/screens/ButtonsScreen.kt
@@ -1,21 +1,26 @@
 package com.ravish.compose.sample.screens
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ravish.compose.customuilibrary.Button
 import com.ravish.compose.customuilibrary.Style
-import com.ravish.compose.customuilibrary.theme.ThemeColor
 
 @Composable
 fun ButtonScreen() {
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(10.dp)) {
         Text(text = "DEFAULT", Modifier.padding(top = 20.dp))
         Row {
             Button(onClick = {}, text = "Submit")
